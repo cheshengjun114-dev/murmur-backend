e70cafbb9d8ec90bb76b6219754e242c5e6e0422
cheshengjun114-dev <cheshengjun114@gmail.com>
2026-05-14 14:50:34 +0900

    init: backend project setup

---
+++ src/main/java/com/murmur/backend/MurmurBackendApplication.java
@@ -0,0 +1,13 @@
+package com.murmur.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MurmurBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MurmurBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/murmur/backend/MurmurBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murmur.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MurmurBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
